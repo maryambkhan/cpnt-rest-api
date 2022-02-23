b230f3f6a7f38d491cdecae9df8c7cc26208e68d
Maryam Khan <maryam.b.khan@edu.sait.ca>
2022-02-22 20:01:40 -0700

    validation

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## Maryam Khan
 
-## rest-api
+## Rest-api
 
 ## Code Journal
 
@@ -18,22 +18,61 @@
   
   - Created button in html 
   - Created a function and called the `fatchData` 
-  - I was having problem. when i refresh button, on html page i had only
+  - I was having problem. When i refresh page on html page i only had
   button it took me while then i realize this that i can call th function 
   once as ` fetchData(endpoint)` and then
   i can call it in button function
+
+   ![Button](images/button.PNG)
+
+   ![Button](images/buttoncode.PNG)
+
   - So i get the page once but it dosent refresh again
+  - I just realize my button is not working Ahhhh !!!!
+  - After so much of try i think i fixed it yey
 
 3. Error handling
 
+- fetch() Promises are handled by a catch block
+-  For 200 OK response 
 
+    ![Try-Catch](images/html.PNG)
 
+    ![Try-Catch](images/console.PNG)
 
+- First i created a function and wrote this piece of code to check if property is :
 
+  if (propertyValue == null || !(typeof propertyValue) === 'string') {
+    console.log(`Property ${propertyValue} is type ${typeof propertyValue}`);
+    throw new Error();
+  }
+  else {
+    console.log(`Property ${propertyValue} is not null and of type string.`);
+  }
 
+- In next step i am going to create an array and put loop to validate all of my properties
+
+     ![Validating](images/loopVal.PNG)
+
+- This method dont work becouse i am not accesing the properties and I am accessing values.
+-  I am getting the result but it is not right
+
+    ![Validating](images/valwithLoop.PNG)
+
+    ![Validating](images/validation.PNG)
+
+- I am not going through whole array and i am just using few properties so it just not make sense.
+- So i am going to check all of the values using 
+
+    checkIfNullOrNotString(selectedMovie.original_title);
 
 ## Attributions
 
-- My own code for button
-- Tony Grimes
 - [Studio Ghibli Api](https://ghibliapi.herokuapp.com/#section/Use-Case)
+- Tony Grimes(starter code for fetching API)
+- My own code for button
+- MDN(Try Catch)
+- Slides(Validating)
+
+- [GH repo](https://github.com/maryambkhan/cpnt-rest-api)
+- [GH Pages](https://maryambkhan.github.io/cpnt-rest-api/)
--- js/main.js
+++ js/main.js
@@ -1,43 +1,91 @@
 const randomIndex = function (length) {
   return Math.floor(Math.random() * length);
 }
+// Fetching Data
 const fetchData = async function (url) {
-  
+  let output = '';
   try {
-     
-        const response = await fetch(url);
-        if (!response.ok) {
-          throw new Error(response.statusText);
-        }
-        const data = await response.json();
-        const character = data[randomIndex(data.length)];
+    const response = await fetch(url);
+    if (!response.ok) {
+      output = `<p>Error retriving data. Got bad response: ${response.status}</p>`;
+      throw new Error(response.statusText);
+    }
+    const ghibliMovies = await response.json();
+    console.log(ghibliMovies);
 
-        console.log(character);
-
-        const output = `
-     <h1>${character.title}</h1>
-     <h2>${character.original_title}</h2>
-     <img src="${character.image}" alt="Images of ${character.title}">
-     <p>${character.description}</p>
-    <p>Producer:  ${character.producer}</p>
-    <p>Director:  ${character.director}</p>
-    <p>
-     <a href="${character.url}">Locations</a>
-    </p>
-      
-      `;
-        document.querySelector('.content').innerHTML = output;
-
-      } catch (error) {
-      console.error("There was problem.");
+    // validating ghibliMovies
+    if (ghibliMovies == null) {
+      console.log(`The selected data was null. List f Ghibli movies: ${ghibliMovies}`);
+      throw new Error();
+    }
+    else if (ghibliMovies.length < 1) {
+      console.log(`The selected data is empty. List f Ghibli movies: ${ghibliMovies}`);
+      throw new Error();
     }
 
+    const selectedMovie = ghibliMovies[randomIndex(ghibliMovies.length)];
+
+    // validating selectedMovie
+    if (selectedMovie == null) {
+      console.log(`The selected data was null. Selected movie: ${selectedMovie}`);
+      throw new Error();
+    }
+    else if (selectedMovie.length < 1) {
+      console.log(`The selected data is empty. Selected movie: ${selectedMovie}`);
+      throw new Error();
+    }
+
+    console.log(selectedMovie);
+    // validation
+    checkIfNullOrNotString(selectedMovie.title);
+    checkIfNullOrNotString(selectedMovie.original_title);
+    checkIfNullOrNotString(selectedMovie.image);
+    checkIfNullOrNotString(selectedMovie.description);
+    checkIfNullOrNotString(selectedMovie.producer);
+    checkIfNullOrNotString(selectedMovie.director);
+    checkIfNullOrNotString(selectedMovie.release_date);
+    checkIfNullOrNotString(selectedMovie.url);
+
+    output = `
+
+     <h1>${selectedMovie.title}</h1>
+     <h2>${selectedMovie.original_title}</h2>
+    <img class="poster" src="${selectedMovie.image}" alt="Images of ${selectedMovie.title}">
+    <section class="article-container">
+    <h2>Description</h2>
+     <p>${selectedMovie.description}</p>
+     </section>
+    <p><strong>Producer:</strong> ${selectedMovie.producer}&emsp;
+    <strong>Director:</strong> ${selectedMovie.director}&emsp;
+    <strong>Released In:</strong> ${selectedMovie.release_date}</p>
+    <p>
+     <a href="${selectedMovie.url}">Locations</a>
+    </p>
+    `;
+
+  } catch (error) {
+    console.error(`Error retriving data. Got bad response: ${error}`);
+  }
+  document.querySelector('.content').innerHTML = output;
+}
+
+const endpoint = 'https://ghibliapi.herokuapp.com/films';
+fetchData(endpoint);
+
+//button
+const next = function () {
+  fetchData(endpoint);
+}
+
+
+// validating 
+const checkIfNullOrNotString = function (propertyValue) {
+  if (propertyValue == null || !(typeof propertyValue) === 'string') {
+    console.log(`Property value ${propertyValue} is type ${typeof propertyValue}`);
+    throw new Error();
+  }
+  else {
+    console.log(`Property value ${propertyValue} is not null and of type string.`);
   }
 
- const endpoint = 'https://ghibliapi.herokuapp.com/films';
- fetchData(endpoint);
-
-  // button
-  const next = function () {
-    fetchData(endpoint);
-  }
+}
